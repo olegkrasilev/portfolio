be72569dcf80a039b681d672f37cfa72a0a1a0b8
olegkrasilev <olegkrasilev@gmail.com>
2022-09-27 13:26:51 +0300

    fix(app): removed unused styles

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,5 +5,6 @@ module.exports = {
   plugins: ["stylelint-order", "stylelint-scss"],
   rules: {
     "selector-class-pattern": "^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$",
+    'no-empty-source': null
   }
 };
