c9e57a35ad7863fbffa6d041e815ec57800676a8
olegkrasilev <olegkrasilev@gmail.com>
2022-09-27 10:24:35 +0300

    fix(app): stylelint rule

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,6 +4,6 @@ module.exports = {
             "stylelint-prettier/recommended"],
   plugins: ["stylelint-order", "stylelint-scss"],
   rules: {
-    // Дополнительные правила
+    "selector-class-pattern": "^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$",
   }
 };
