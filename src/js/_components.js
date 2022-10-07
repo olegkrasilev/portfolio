a93bda0a7a4eb7c77ebd0c7b7e51a7cc49604066
olegkrasilev <olegkrasilev@gmail.com>
2022-10-07 11:34:23 +0300

    feat(app): preloader

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,1 +1,16 @@
-console.log('components');
+
+
+
+/*
+  Preloader
+*/
+
+const preloader = document.getElementById('preloader')
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  setTimeout(() => {
+    preloader.classList.add('loaded')
+  },4000)
+},{once: true});
