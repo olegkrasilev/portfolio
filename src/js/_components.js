472a99d04755653e9abee3291db31cfdd02f2948
olegkrasilev <olegkrasilev@gmail.com>
2022-10-10 12:00:26 +0300

    feat(app): navigation

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,16 +1,22 @@
-
-
-
 /*
   Preloader
 */
 
 const preloader = document.getElementById('preloader')
 
-
-
 document.addEventListener('DOMContentLoaded', (event) => {
   setTimeout(() => {
     preloader.classList.add('loaded')
   },4000)
 },{once: true});
+
+/* Burger Menu Logic */
+
+const burger = document.getElementById('burger');
+const burgerFirstRect = document.getElementById('burgerFirstRect');
+const burgerSecondRect = document.getElementById('burgerSecondRect');
+
+burger.addEventListener('click',() => {
+  burgerFirstRect.classList.toggle('firstRect');
+  burgerSecondRect.classList.toggle('secondRect');
+})
