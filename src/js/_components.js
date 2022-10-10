c11aafe9e43c24c273217a6d96581eb62ce95ef0
olegkrasilev <olegkrasilev@gmail.com>
2022-10-10 16:25:38 +0300

    feat(app): mobile menu

--- src/js/_components.js
+++ src/js/_components.js
@@ -4,19 +4,29 @@
 
 const preloader = document.getElementById('preloader')
 
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
   setTimeout(() => {
     preloader.classList.add('loaded')
   },4000)
 },{once: true});
 
 /* Burger Menu Logic */
+const bodyScrollLock = require('body-scroll-lock');
+const { disableBodyScroll,  enableBodyScroll } = bodyScrollLock;
 
 const burger = document.getElementById('burger');
 const burgerFirstRect = document.getElementById('burgerFirstRect');
 const burgerSecondRect = document.getElementById('burgerSecondRect');
+const menu = document.getElementById('menu');
 
 burger.addEventListener('click',() => {
   burgerFirstRect.classList.toggle('firstRect');
   burgerSecondRect.classList.toggle('secondRect');
+  menu.classList.toggle('active');
+
+  if(menu.classList.contains('active')) {
+    disableBodyScroll(menu)
+  } else {
+    enableBodyScroll(menu)
+  }
 })
